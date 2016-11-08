869c0805ff8400cb0446b0715650113410339046
amattag <amattag@gmail.com>
2016-11-08 20:12:04 +0000

    Input arguments are received from console

--- motionPred/clustering.py
+++ motionPred/clustering.py
@@ -10,24 +10,38 @@ Expectation-Maximization (E-M) algorithm.
 # Import required modules.
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import random
 
 # import auxiliar modules.
 import expMax as em
 import plotting as myplt
 import probability as pobty
 
+# Check if the correct input is given in command-line arguments.
+if len(sys.argv) != 5:
+	print('Usage: ./clustering.py tjcsN clustersN maxIter tol')
+	sys.exit(1)         # Return a non-zero value for abnormal termination
+
+# Load arguments into variables.
+print 'Argument List:', str(sys.argv)
+tjcsN = int(sys.argv[1])     # Number of Trajectories to load.
+clustersN = int(sys.argv[2]) # Number of Clusters.
+maxIter = int(sys.argv[3])   # Maximum number of iterations for the E-M algoritm.
+tol = float(sys.argv[4])     # error tolerance for the E-M algorithm.
+
 # Load trayectory data from the 'tjcs.npy' file.
 # A set of 1856 trajectories generated from the Edinburgh Informatics Forum 
 # Pedestrian Database: http://homepages.inf.ed.ac.uk/rbf/FORUMTRACKING/
 data = np.load("data/tjcs.npy")
 
-# Choose the first 500 trajectories.
-tjcs = data[:200]
+# Choose the first number of trajectories given by the tjcsN argument.
+tjcs = data[:tjcsN]
 
 # Plot selected trajectories
 fig1 = plt.figure()
 wm = plt.get_current_fig_manager()
-wm.window.wm_geometry("600x500+50+50")
+wm.window.wm_geometry("600x500+200+0")
 fig1 = plt.title("Trajectories")
 plt.axis([0,16,0,12])
 plt.xlabel('X (mts)')
@@ -35,25 +49,28 @@ plt.ylabel('Y (mts)')
 myplt.plot_trajectories(tjcs)
 plt.pause(0.05)
 
-# Get number of trajectories and the lenght of every of them.
-tjcsN = tjcs.shape[0]         # Number of Trajectories. 
-tjcsLength = tjcs.shape[1]    # Lenght of each trajectory.
+# Get the length of each trajectory.
+tjcsLength = tjcs.shape[1]
    
+# Select intial means for E-M algorithm.
+means = np.zeros((clustersN, tjcsLength, 2))
+meansIndex = random.sample(np.arange(tjcsN), clustersN)
+for m, n in zip( xrange(clustersN), meansIndex):
+	means[m] = data[n]
+
 # Select intial means and best number of clusters.
-means, meansIndexTmp = em.selectClusters(tjcs, tjcsN, tjcsLength)
-clustersN = means.shape[0]
-meansIndex = [i[0] for i in meansIndexTmp]
+#means, meansIndexTmp = em.selectClusters(tjcs, tjcsN, tjcsLength)
+#clustersN = means.shape[0]
+#print "clustersN", clustersN
+#meansIndex = [i[0] for i in meansIndexTmp]
 
 # Compute covariance Matrix.
 vtjcs=tjcs.reshape((tjcsN*tjcsLength, tjcs.shape[2]))
 covariance = np.round(np.cov(vtjcs.T)*np.eye(vtjcs.shape[1]))
 
 ### Let's iterate with the E-M algorithm ###
-maxIter = 200
-tol = 0.001
 ll_old = 0
 i = 0
-#for i in xrange(maxIter):
 while i < maxIter:
 	# E-M algorithm.
     clusters = em.expectation(tjcs, means, covariance, pobty.t_gaussian)
@@ -98,7 +115,7 @@ while i < maxIter:
 # This is to plot the results of the E-M algorithm.
 fig2 = plt.figure()
 wm = plt.get_current_fig_manager()
-wm.window.wm_geometry("600x500+700+50")
+wm.window.wm_geometry("600x500+850+0")
 plt.axis([0,16,0,12])
 plt.xlabel('X (mts)')
 plt.ylabel('Y (mts)')
@@ -112,7 +129,7 @@ np.save("data/clusters.npy", means)
 # Let's plot the found clusters.
 fig3 = plt.figure()
 wm = plt.get_current_fig_manager()
-wm.window.wm_geometry("600x500+350+200")
+wm.window.wm_geometry("600x500+200+600")
 fig3 = plt.title("Clusters")
 plt.grid()
 plt.xticks(np.arange(0, 16, 1.0))
@@ -120,12 +137,13 @@ plt.yticks(np.arange(0, 12, 1.0))
 plt.xlabel('X (mts)')
 plt.ylabel('Y (mts)')
 fig3 = myplt.plot_time_model(means)
+#fig3 = myplt.plot_trajectories(means)
 
 # Plot cluster contribs
 cluster_contribs = np.sum(clusters, 0)
 fig4 = plt.figure()
 wm = plt.get_current_fig_manager()
-wm.window.wm_geometry("600x500+350+200")
+wm.window.wm_geometry("600x500+850+600")
 fig4 = plt.title("Number of Trajectories per Cluster")
 fig4 = plt.bar(np.arange(clustersN), cluster_contribs, color = myplt.generate_palette(clustersN))
 plt.show()
