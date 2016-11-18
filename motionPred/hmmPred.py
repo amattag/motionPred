5e329c14d4fac248250c41a64ae50a1f0fabc58e
amattag <amattag@gmail.com>
2016-11-18 17:01:37 +0000

    module adapted to new plotting library

--- motionPred/hmmPred.py
+++ motionPred/hmmPred.py
@@ -18,16 +18,8 @@ import plotting as myplt
 clusters = np.load("data/clusters.npy")
 
 # Let's plot those clusters again.
-wm = plt.get_current_fig_manager()
-wm.window.wm_geometry("700x600+300+70")
-plt.title("Clusters")
-plt.grid()
-plt.xticks(np.arange(0, 16, 1.0))
-plt.yticks(np.arange(0, 12, 1.0))
-plt.xlabel('X (mts)')
-plt.ylabel('Y (mts)')
-myplt.plot_time_model(clusters)
-plt.show()
+fig1 = plt.figure()
+myplt.plot_time_model(clusters, "Clusters", "600x500+200+0")
 
 # Resample the original clusters every two samples (0.6 seconds).
 clustersN, origSteps, dim = clusters.shape
@@ -43,16 +35,7 @@ for m in xrange(clustersN):
 
 # Plot the new clusters
 fig2 = plt.figure()
-wm = plt.get_current_fig_manager()
-wm.window.wm_geometry("700x600+300+70")
-fig2 = plt.title("Clusters")
-plt.grid()
-plt.xticks(np.arange(0, 16, 1.0))
-plt.yticks(np.arange(0, 12, 1.0))
-plt.xlabel('X (mts)')
-plt.ylabel('Y (mts)')
-myplt.plot_time_model(means)
-plt.show()
+myplt.plot_time_model(means, "Clusters", "600x500+850+0")
 
 ### Let us create the HMM model. ###
 
@@ -77,12 +60,12 @@ pi /= np.sum(pi)
 
 # Third step: The global covariance matrix for the observation probabilities.
 covariance = np.array( [[4.0, 0.0], [0.0, 4.0]] )
-cov_inv = np.linalg.inv( covariance )
+invCov= np.linalg.inv(covariance)
 
 ### Using the HMM model for prediction. ###
 
 # The Filtering algorithm.
-def filter(belief, A, means, cov_inv, obs, obsStep):
+def filter(belief, A, means, invCov, obs, obsStep):
     """filter: To estimate the probability distribution over the state at the
        current time.
     
@@ -103,26 +86,15 @@ def filter(belief, A, means, cov_inv, obs, obsStep):
     obs: array
       Trajectory array used as an observation.
       
-    step: int
+    obsStep: int
       State step of the coming observation.
     """
-    #clustersN, new_t = belief.shape
-    #current = np.zeros((clustersN, new_t))
-    #for m in xrange(clustersN):
-        #for i in xrange(new_t):
-            #for old_t in xrange(new_t):
-                #diff = means[m, i] - obs[step]
-                #expo = -0.5 * np.dot(np.dot(diff, cov_inv), diff.transpose())
-                #current[m, i] += belief[m, old_t] * A[m, old_t, i] * np.exp(expo) 
-    #current /= np.sum(current)
-    #belief[:] = current[:]
-    
     clustersN, stepsN = belief.shape
     currentState = np.zeros((clustersN, stepsN))
     for m in xrange(clustersN):
         for currentStep in xrange(stepsN):
 			diff = means[m, currentStep] - obs[obsStep]
-			expo = -0.5 * np.dot(np.dot(diff, cov_inv), diff.transpose())
+			expo = -0.5 * np.dot(np.dot(diff, invCov), diff.transpose())
 			for prevStep in xrange(stepsN):
 				currentState[m, currentStep] += belief[m, prevStep] * A[m, prevStep, currentStep] * np.exp(expo) 
     currentState /= np.sum(currentState)
@@ -167,24 +139,22 @@ data = np.load("data/tjcs.npy")
 test_data = data[-100:]
 
 iniState = 0    # initial State
-endState = stepsN   # final State
 interval = 2    # State Step interval
 stepsAhead = 10 # Number of steps ahead in the future
+endState = stepsN   # final State
 
-for obs in test_data[34:37]:  # Use tjcs 34 to 36 as observations. 
+# Make predictions using a set of observations from the test data.
+obsIni = 34  # Initial observation
+obsFin = 37  # Final observation
+for obs in test_data[obsIni:obsFin]:   
     belief = pi.copy()
     for obsStep in xrange(iniState, (endState+1), interval):
-        filter(belief, A, means, cov_inv, obs, obsStep)
-        prediction = predict(belief, A, stepsAhead)
-        
-        #Plot prediction
-        wm = plt.get_current_fig_manager()
-        wm.window.wm_geometry("700x600+300+70")
-        plt.grid()
-        plt.xticks(np.arange(0, 16, 1.0))
-        plt.yticks(np.arange(0, 12, 1.0))
-        plt.xlabel('X (mts)')
-        plt.ylabel('Y (mts)')  
-        plt.title("Prediction. State Step: %s" %obsStep)
-        myplt.plot_prediction(means, obs[:obsStep], belief, prediction)        
+        filter(belief, A, means, invCov, obs, obsStep)
+        prediction = predict(belief, A, stepsAhead)       
+        # Plot prediction
+        fig3 = plt.figure()
+        myplt.plot_prediction(means, obs[:obsStep], obsStep, obsIni, belief, prediction)
+        #plt.pause(0.05)
         plt.show()
+    obsIni +=1
+        
